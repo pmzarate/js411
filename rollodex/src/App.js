dcf0bad3f936f05ced1c122f9a5135f01bb258b4
pmzarate <patriciamarakzarate@yahoo.com>
2019-11-08 13:35:25 -0600

    final Rollodex

--- rollodex/src/App.js
+++ rollodex/src/App.js
@@ -1,13 +1,18 @@
 import React, {Component} from 'react';
 import './App.css';
-import Details from './details';
+//import ReactDOM from 'react-dom';
+//import Button from '@material-ui/core';
+//import  {Card, CardActions, CardContent} from '@material-ui/core';
+//import Typography from '@material-ui/core';
+
 
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      peopleList : [],
-    }
+      contacts : [],
+      hidden: []
+    };
   }
 
 componentDidMount() {
@@ -17,38 +22,39 @@ componentDidMount() {
 fetchData() {
       fetch('https://randomuser.me/api?results=25')
       .then (results =>results.json())
-      // .then (parsedJSON => return parsedJSON.results()))
-      .then (parsedJSON => this.setState({peopleList : parsedJSON.results}) )
+      .then (parsedJSON => this.setState({contacts : parsedJSON.results}) )
       .catch(error => console.log("error", error))
 }
 
+toggleDetails = (uuid) => {
+  let newHidden = {...this.state.hidden};
+  newHidden[uuid] = !newHidden[uuid]
+  this.setState({ hidden: newHidden});
+}
+
 render() {
-  //console.log(this.state.peopleList)
+    let { contacts, hidden } = this.state;
     return (
-      <div>
-        <header>
-          <h1>Rollodex</h1>
-        </header>
-        <br/>
-          <ul>
-            {this.state.peopleList.map(function(person,index) {
-              return (
-                <div key = {index}>
+      <div className = "App">
+        <header><h1>Rollodex</h1></header>
+        {contacts.map( (c) => (
+          <div>
+            <ul>
+              <br/><br/>
+                <img src = {c.picture.thumbnail} alt = "person_image" width="90px" height="90px"/>
+                    <h2><div> {c.name.first} {c.name.last}</div> </h2><br/> 
+                      <button onClick = {() => this.toggleDetails(c.login.uuid)} > {hidden[c.login.uuid] ? "Hide Details" : "Show Details"} </button>
+                    <br/> <br/>
+                    {hidden[c.login.uuid] && <div> <p> {c.location.street.number} {c.location.street.name} <br/> {c.location.city}, {c.location.state}{c. location.postcode}<br/>{c.location.country}<br/> Contact Cell#: {c.cell} <br/> Email: {c.email}</p></div>}
+                    <br/>
                   <br/>
-                  <br/>
-                    <img src = {person.picture.thumbnail} alt = "person_image" width="90px" height="90px"/>
-                    <h2> {person.name.first} {person.name.last} <br/> <Details/>
-                  </h2>
-                  <br/>
-                  <br/>
-              </div>
-              )
-            })}
-          </ul>
+                  </ul>
+          </div>
+          ))}
       </div>
     )
-  }
-};
+  }}
 
-
+  // const rootElement = document.getElementById("root");
+  // ReactDOM.render(<App />, rootElement); 
 export default App
